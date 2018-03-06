1a91b66b3db9525159724f462aad10751e9d0f50
Ben Gamari <ben@smart-cactus.org>
2018-03-06 11:22:23 -0500

    Add pyqt5 to scipyEnv

--- overlay.nix
+++ overlay.nix
@@ -75,7 +75,7 @@ rec {
 
   ben = {
     scipyEnv = self.python3.withPackages (ps: with ps; [
-      ipython numpy matplotlib scipy pygobject3
+      ipython numpy matplotlib scipy pygobject3 pyqt5
     ]);
   };
 }
